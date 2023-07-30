49fc4caea84470143f7be768288e9c69455f017b
Purwa Aditya Wisnu Wardhana <adityawisnu104@gmail.com>
2023-07-30 22:03:48 +0700

    init tailwind element

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./index.html",
     "./src/**/*.{html,js}",
     "./node_modules/tw-elements/dist/js/**/*.js",
+    "./node_modules/tw-elements-react/dist/js/**/*.js",
   ],
-  plugins: [require("tw-elements/dist/plugin.cjs")],
+  plugins: [
+    require("tw-elements/dist/plugin.cjs"),
+    require("tw-elements-react/dist/plugin.cjs"),
+  ],
   darkMode: "class",
 };
